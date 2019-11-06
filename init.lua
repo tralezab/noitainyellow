919599c2fa79cc453c1910ca6d29ace4bc8905c4
tralezab <spamqetuo2@gmail.com>
2019-11-06 13:02:21 -0800

    fix the sprite

--- init.lua
+++ init.lua
@@ -8,11 +8,16 @@ function OnPlayerSpawned( player_entity ) -- this runs when player entity has be
 	end
 	GameAddFlagRun( init_check_flag )
 
-
-	local inventory = nil
 	local x,y = EntityGetTransform( player_entity )
 
-	-- find the quick inventory
+	local inventory = nil
+	local cape = nil
+	local player_arm = nil
+	
+	local loadout_cape_color = loadout_choice.cape_color
+	local loadout_cape_color_edge = loadout_choice.cape_color_edge
+
+	-- find the quick inventory, player cape and arm
 	local player_child_entities = EntityGetAllChildren( player_entity )
 	if ( player_child_entities ~= nil ) then
 		for i,child_entity in ipairs( player_child_entities ) do
@@ -20,6 +25,14 @@ function OnPlayerSpawned( player_entity ) -- this runs when player entity has be
 			
 			if ( child_entity_name == "inventory_quick" ) then
 				inventory = child_entity
+			end
+			
+			if ( child_entity_name == "cape" ) then
+				cape = child_entity
+			end
+			
+			if ( child_entity_name == "arm_r" ) then
+				player_arm = child_entity
 			end
 		end
 	end
